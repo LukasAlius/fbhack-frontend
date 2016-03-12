af4d2db6400d32e1a80a8e3bbc5e9bddac3691c3
Anirudh <anirudhx5@gmail.com>
2016-03-12 20:19:04 +0000

    react

--- playlistController.js
+++ playlistController.js
@@ -1,13 +1,30 @@
 
 function addRow(title, fline, sline) {
     var div = document.createElement('li');
-            div.className = 'collection-item avatar hoverable';
-            div.innerHTML = '<i class="material-icons circle red">play_arrow</i><span class="title">'+title+'</span><p>'+fline+'<br>'+ sline +'</p> <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>';
-            document.getElementById('collection').appendChild(div);
+    div.className = 'collection-item avatar hoverable';
+
+    var ione = document.createElement('i');
+    ione.className = "material-icons circle red";
+    ione.innerHTML = 'play_arrow';
+    ione.addEventListener('click', function(e){
+      var target = e.target;
+    })
+    div.appendChild(ione);
+
+    var span = document.createElement('span');
+    span.className = 'title';
+    span.innerHTML = title;
+    div.appendChild(span);
+
+    var p = document.createElement('p');
+    p.innerHTML = fline+'<br>'+ sline
+    div.appendChild(p);
+
+    document.getElementById('collection').appendChild(div);
 }
 
 for (i = 0; i < 7; i++) {
-          addRow('Song title', 'Artist', 'Description');
+  addRow('Song title', 'Artist', 'Description');
 }
 
 function play(){
