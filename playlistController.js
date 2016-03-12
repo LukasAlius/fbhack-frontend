d7683d79c1afb637a76ba73bac0e14dd22679d52
Luxs <lukasalius@gmail.com>
2016-03-12 17:29:58 +0000

    updated playlistController

--- playlistController.js
+++ playlistController.js
@@ -2,14 +2,17 @@
 function addRow(title, fline, sline) {
     var div = document.createElement('li');
             div.className = 'collection-item avatar hoverable';
-            div.innerHTML = '<i class="material-icons circle red">play_arrow</i><span class="title">'+title+'</span><p>'+fline+'<br>'+ sline +'</p> <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>';
+            div.innerHTML = '<i id="play" class="material-icons circle red">play_arrow</i><span class="title">'+title+'</span><p>'+fline+'<br>'+ sline +'</p> <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>';
             document.getElementById('collection').appendChild(div);
 }
 
-for (i = 0; i < 7; i++) {
+function playMusic(){
+    alert("PlayMusic");
+    var elem = document.getElementById('play').innerHTML = "pause";
+}
+
+for (i = 0; i < 17; i++) {
           addRow('Song title', 'Artist', 'Description');
 }
 
-function play(){
-
-}
+playMusic();
