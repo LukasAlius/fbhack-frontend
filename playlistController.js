7e40efe5f74018842bc1c2b4cf4b69225655e90e
Anirudh <anirudhx5@gmail.com>
2016-03-12 17:01:12 +0000

    pulling

--- playlistController.js
+++ playlistController.js
@@ -1,14 +1,14 @@
 function addRow() {
-    var div = document.createElement('li');
-    div.className = 'collection-item avatar hoverable';
-    div.innerHTML = '<i class="material-icons circle red">play_arrow</i><span class="title">Title</span><p>First Line <br> Second Line </p> <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>';
-    document.getElementById('collection').appendChild(div);
+  var div = document.createElement('li');
+  div.className = 'collection-item avatar hoverable';
+  div.innerHTML = '<i class="material-icons circle red">play_arrow</i><span class="title">Title</span><p>First Line <br> Second Line </p> <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>';
+  document.getElementById('collection').appendChild(div);
 }
 
 function removeRow(input) {
-    document.getElementById('collection').removeChild( input.parentNode );
+  document.getElementById('collection').removeChild( input.parentNode );
 }
 
 for (i = 0; i < 7; i++) {
-          addRow();
+     addRow();
 }
